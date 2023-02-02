bd49d66335c2868f85d8c1b80cba6c67303c9ffe
petelpop <m.naufal.s.k@gmail.com>
2023-02-02 14:26:24 +0700

    add delete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,26 +13,44 @@
 @endif
   <div class="card">
       <div class="card-body">
-          <form action="{{url('/tasks')}}" method="POST">
+          <form action="{{ route('register') }}" method="POST">
             @csrf
               <div class="mb-3">
-                  <label for="" class="form-label">User</label>
-                  <input name="user" type="text" class="form-control" value="{{ old('user') }}">
-                  @error('user')
+                  <label for="" class="form-label">Name</label>
+                  <input name="name" type="text" class="form-control" value="{{ old('name') }}">
+                  @error('name')
                       <span class="text-danger">
                         {{ $message }}
                       </span>
                   @enderror
               </div>
               <div class="mb-3">
-                  <label for="" class="form-label">Task</label>
-                  <textarea name="tasks" class="form-control" id="" rows="3">{{ old('task') }}</textarea>
-                  @error('tasks')
-                  <span class="text-danger">
-                    {{ $message }}
-                  </span>
-              @enderror
-              </div>
+                <label for="" class="form-label">Email</label>
+                <input name="email" type="email" class="form-control" value="{{ old('email') }}">
+                @error('email')
+                    <span class="text-danger">
+                      {{ $message }}
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Password</label>
+                <input name="password" type="text" class="form-control" value="{{ old('password') }}">
+                @error('password')
+                    <span class="text-danger">
+                      {{ $message }}
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Confirm Password</label>
+                <input name="password_confirmation" type="password" class="form-control">
+                @error('password')
+                    <span class="text-danger">
+                      {{ $message }}
+                    </span>
+                @enderror
+            </div>
               <button type="submit" class="btn btn-primary">Submit</button>
           </form>
       </div>
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('main')
-<div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 500px;">
+<div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 700px;">
     <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
         <span class="fs-5 fw-semibold">Task Lists: {{ $data-> total()}}</span>
         <a href="{{url('/tasks/create')}}" class="btn btn-sm btn-primary">add</a>
