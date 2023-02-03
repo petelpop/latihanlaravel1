9195f61385812c3495d55438ca59414f3b5f4491
petelpop <m.naufal.s.k@gmail.com>
2023-02-03 10:07:40 +0700

    adding middleware admin role

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/tasks/create', [TaskController::class, 'create']);
-Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks', [TaskController::class, 'index'])->middleware('is_admin');
 Route::get('/tasks/{id}',[TaskController::class, 'show'] );
 Route::post('/tasks/', [TaskController::class, 'store']);
 Route::get('/tasks/{id}/edit', [TaskController::class, 'edit']);
